5a3c5c3031c406f8ee3153d598d73eeff4b7bb17
Marco Giammarini <m.giammarini@warcomeb.it>
2020-12-27 23:32:24 +0100

    .

---
+++ config.h
@@ -0,0 +1,53 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <QString>
+
+/*!
+ * \brief The BOM_Format enum
+ */
+enum BOMFormat
+{
+    BOM_FORMAT_JSON,
+    BOM_FORMAT_HTML,
+    BOM_FORMAT_CSV,
+};
+
+/*!
+ * \brief The Config struct
+ */
+struct Config
+{
+    QString path;
+
+    QString schematic;
+
+    // Parse component model
+    bool onlyDefault;
+
+    /*!
+     * \brief boardName
+     */
+    QString boardName;
+
+    /*!
+     * \brief outputModel
+     */
+    QString outputModel;
+
+    /*!
+     * \brief styleFile
+     */
+    QString styleFile;
+
+    QString outputFile;
+    BOMFormat format;
+
+    /*!
+     * \brief verboseLevel
+     */
+    quint8 verboseLevel;
+};
+
+
+#endif // CONFIG_H
