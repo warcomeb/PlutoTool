7583e2f040690693825bac0449211ae42942b831
Marco Giammarini <m.giammarini@warcomeb.it>
2021-01-23 15:10:03 +0100

    Implemented Scheduled class.

--- scheduled.cpp
+++ scheduled.cpp
@@ -20,7 +20,111 @@
 
 #include "scheduled.h"
 
-Scheduled::Scheduled()
+Scheduled::Scheduled ():
+    mPayee(),
+    mDeadline(),
+    mAmount(0.0),
+    mWorkOrder(),
+    mCategory(),
+    mPaid(false),
+    mNote("")
+{
+}
+
+Scheduled::Scheduled (Payee payee,
+                      QDate deadline,
+                      float amount,
+                      WorkOrder workorder,
+                      Category category):
+    mPayee(payee),
+    mDeadline(deadline),
+    mAmount(amount),
+    mWorkOrder(workorder),
+    mCategory(category),
+    mPaid(false),
+    mNote("")
 {
 
+}
+
+void Scheduled::setId (quint32 id)
+{
+    mId = id;
+}
+
+quint32 Scheduled::id (void)
+{
+    return mId;
+}
+
+void Scheduled::setNote (QString note)
+{
+    mNote = note;
+}
+
+QString Scheduled::note (void)
+{
+    return mNote;
+}
+
+void Scheduled::write (QJsonObject &json) const
+{
+    QJsonObject o;
+
+    o["Id"]          = (int)mId;
+    o["Payee"]       = (int)mPayee.id();
+    o["Amount"]      = mAmount;
+    o["Category"]    = (int)mCategory.id();
+    o["WorkOrder"]   = (int)mWorkOrder.id();
+    o["Deadline"]    = mDeadline.toString("yyyy-MM-dd");
+    o["Paid"]        = mPaid;
+    o["Note"]        = mNote;
+
+    json.insert("Scheduled", o);
+}
+
+void Scheduled::read (const QJsonObject &json,
+                        QMap<quint32,Payee> payees,
+                        QMap<quint32,Category> categories,
+                        QMap<quint32,WorkOrder> workorders)
+{
+    if (json.contains("Id") && json["Id"].isDouble())
+    {
+        mId = json["Id"].toInt();
+    }
+
+    if (json.contains("Payee") && json["Payee"].isDouble())
+    {
+        mPayee = payees[json["Payee"].toInt()];
+    }
+
+    if (json.contains("Amount") && json["Amount"].isDouble())
+    {
+        mAmount = json["Amount"].toDouble();
+    }
+
+    if (json.contains("Category") && json["Category"].isDouble())
+    {
+        mCategory = categories[json["Category"].toInt()];
+    }
+
+    if (json.contains("WorkOrder") && json["WorkOrder"].isDouble())
+    {
+        mWorkOrder = workorders[json["WorkOrder"].toInt()];
+    }
+
+    if (json.contains("Deadline") && json["Deadline"].isString())
+    {
+        mDeadline = QDate::fromString(json["Deadline"].toString(),"yyyy-MM-dd");
+    }
+
+    if (json.contains("Note") && json["Note"].isString())
+    {
+        mNote = json["Note"].toString();
+    }
+
+    if (json.contains("Paid") && json["Paid"].isBool())
+    {
+        mPaid = json["Paid"].toBool();
+    }
 }
--- scheduled.h
+++ scheduled.h
@@ -21,11 +21,52 @@
 #ifndef SCHEDULED_H
 #define SCHEDULED_H
 
+#include <QtGlobal>
+#include <QString>
+#include <QDate>
+
+#include "transaction.h"
+#include "category.h"
+#include "workorder.h"
+#include "payee.h"
 
 class Scheduled
 {
 public:
-    Scheduled();
+    Scheduled ();
+    Scheduled (Payee payee,
+               QDate deadline,
+               float amount,
+               WorkOrder workorder,
+               Category category);
+
+    void setId (quint32 id);
+    quint32 id (void);
+
+    void setNote (QString note);
+    QString note (void);
+
+    void write (QJsonObject &json) const;
+    void read (const QJsonObject &json,
+               QMap<quint32,Payee> payees,
+               QMap<quint32,Category> categories,
+               QMap<quint32,WorkOrder> workorders);
+
+private:
+    quint32   mId;
+
+    Payee     mPayee;
+
+    QDate     mDeadline;
+    float     mAmount;
+
+    WorkOrder mWorkOrder;
+
+    Category  mCategory;
+
+    bool      mPaid;
+
+    QString   mNote;
 };
 
 #endif // SCHEDULED_H
